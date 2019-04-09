2272b4101c2d905d619256c517001ebdd1d4e602
Josiah Chapman <33184352+josiahchapman@users.noreply.github.com>
2019-04-09 16:33:56 -0500

    Created Swift project and included XD (UI) files in UI folder.

---
+++ iOS/_SQL_Social/_SQL_Social/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  _SQL_Social
+//
+//  Created by Josiah Chapman on 4/9/19.
+//  Copyright © 2019 Josiah Chapman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS/_SQL_Social/_SQL_Social/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  _SQL_Social
+//
+//  Created by Josiah Chapman on 4/9/19.
+//  Copyright © 2019 Josiah Chapman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
