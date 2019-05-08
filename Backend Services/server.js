2475cc8c7397d6495ab826351396060669567e30
DiontrePate <zdpate@tiu.edu>
2019-05-07 20:43:30 -0500

    helo

--- Backend Services/server.js
+++ Backend Services/server.js
@@ -31,7 +31,7 @@ var server = http.createServer(function (req, res)
   {
     // for /register-email-password-firstname-Last_Name
 
-    var query1 = " insert into user (Email, Password, First_Name, Last_Name)"
+    var query1 = " insert into APP_USER (Email, PASSWORD_HASH, First_Name, Last_Name)"
                 + "values('"+dataReq[1]+"', '"+dataReq[2]+"', '"+dataReq[3]+"','"+dataReq[4]+"')";
 
 
@@ -40,16 +40,56 @@ var server = http.createServer(function (req, res)
                         if (err) console.log(err);
                     });
 
-     //check the URL of the current request
            res.writeHead(200, { 'Content-Type': 'application/json' });
-      //     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-           res.write(JSON.stringify({ message: "Hello World"}));
+           res.write(JSON.stringify({ message: "QUERY SUCCESSFUL"}));
       //     res.write('<html><body><p>This is home Page.</p></body></html>');
 
           // res.end();
          }
-  //else if (dataReq)
+
+       else if (dataReq[0] == '/CreatePost')
+         {
+        // for /CreatePost-id-vote_count-Title-Answer_count-body
+
+           var query1 = " insert into POST ( id , vote_count, Title, Answer_Count, Body)"
+                       + "values('"+dataReq[1]+"', '"+dataReq[2]+"', '"+dataReq[3]+"','"+dataReq[4]+"','"+dataReq[5]+"')";
+
+
+                           conn.query(query1, function(err, results)
+                            {
+                               if (err) console.log(err);
+                           });
+
+                  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+                  res.write(JSON.stringify({ message: "QUERY SUCCESSFUL"}));
+             //     res.write('<html><body><p>This is home Page.</p></body></html>');
+
+                 // res.end();
+                }
+
+                          else if (dataReq[0] == '/CreateComment')
+                  {
+                 // for /CreatePost-USER_EMAIL-POST_ID-BODY
+
+                    var query1 = " insert into ANSWERS ( USER_EMAIL, POST_ID, BODY)"
+                                + "values('"+dataReq[1]+"', '"+dataReq[2]+"', '"+dataReq[3]+"')";
+
+
+                                    conn.query(query1, function(err, results)
+                                     {
+                                        if (err) console.log(err);
+                                    });
+
+                           res.writeHead(200, { 'Content-Type': 'application/json' });
+
+                           res.write(JSON.stringify({ message: "QUERY SUCCESSFUL"}));
+                      //     res.write('<html><body><p>This is home Page.</p></body></html>');
+
+                          // res.end();
+                         }
+
 
 
     else
